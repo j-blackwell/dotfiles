a6e959c7ec3e6dff3630651560ffd2a760c6474f
james <james@MacBook-Pro.local>
2023-12-13 23:26:22 +0000

    remove vscode

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,5 @@
 DOTFILES=$(pwd -P)
 
 ln -s $DOTFILES/config/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILES/config/.vscode $HOME/.vscode
 ln -s $DOTFILES/config/.ipython $HOME/.ipython
 ln -s $DOTFILES/config/.zshrc $HOME/.zshrc
