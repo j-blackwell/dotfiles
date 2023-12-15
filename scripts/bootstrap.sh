a72858bc134808a1145846c1158e7121f4a56926
James Robinson <james@ember-climate.org>
2023-12-15 23:27:13 +0000

    add vscode settings

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,3 +3,5 @@ DOTFILES=$(pwd -P)
 ln -s $DOTFILES/config/.gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/config/.ipython $HOME/.ipython
 ln -s $DOTFILES/config/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/config/.config/Code/User $HOME/.config/Code/User
+ln -s $DOTFILES/config/.config/Code/User $HOME/.config/Code/User
