2712d63505aee1cd8b4b2278abb204ce44ed4612
James Robinson <33688964+jrstats@users.noreply.github.com>
2023-12-29 17:26:14 +0000

    change ytm to .deb

--- scripts/install/apps/ytmdesktop.sh
+++ scripts/install/apps/ytmdesktop.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+wget https://github.com/ytmdesktop/ytmdesktop/releases/download/v2.0.0-rc.4/youtube-music-desktop-app_2.0.0.rc.4_amd64.deb
 
-sudo snap install youtube-music-desktop-app
+sudo dpkg -i youtube-music-desktop-app_*_amd64.deb
+sudo apt --fix-broken install
+
+rm youtube-music-desktop-app_*_amd64.deb
