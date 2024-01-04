eb0b70273427f43ea8cd5f05873d6164c7ffcb09
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-01-04 10:01:21 +0000

    add config linking to setup

--- scripts/install/setup/setup.sh
+++ scripts/install/setup/setup.sh
@@ -12,3 +12,11 @@ sudo apt install snapd
 ## .bashrc
 ln $DOTFILES/config/.bashrc $HOME/.bashrc
 ln $DOTFILES/config/.gitconfig $HOME/.gitconfig
+ln $DOTFILES/config/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln $DOTFILES/config/.ipython/profile_default/ipython_config.py $HOME/.ipython/profile_default/ipython_config.py
+ln $DOTFILES/config/.ipython/profile_default/startup/disable-warnings.py $HOME/.ipython/profile_default/startup/disable-warnings.py
+
+
+
+## TODO: folders
+#ln $DOTFILES/config/.config/synth-shell
