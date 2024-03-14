67495a2fea1fdc0ba29925c51b12cd70157ce202
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-03-14 19:05:19 +0000

    add lazyvim and fonts

--- scripts/install/apps/neovim.sh
+++ scripts/install/apps/neovim.sh
@@ -7,8 +7,15 @@ sudo apt install xclip
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
 rm nvim-linux64.tar.gz
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit
+rm lazygit.tar.gz
 
-git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim/
+rm -rf ~/.config/nvim/.git
---
+++ scripts/install/fonts/fonts.sh
@@ -0,0 +1,5 @@
+cd ~/Downloads/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/MartianMono.zip
+unzip -d MartianMono MartianMono.zip
+cp -r MartianMono ~/.local/share/fonts/
+rm -rf MartianMono
