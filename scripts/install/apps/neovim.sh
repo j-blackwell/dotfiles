37efe411ff060f52b2a2e0d9c1fdeb928b11c4c8
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-03-17 20:27:10 +0000

    update neovim.sj

--- scripts/install/apps/neovim.sh
+++ scripts/install/apps/neovim.sh
@@ -4,18 +4,23 @@
 sudo apt-get install ripgrep
 sudo apt install xclip
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
-rm nvim-linux64.tar.gz
-
+# dependencies - lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm lazygit
 rm lazygit.tar.gz
+
+# dependencies - npm
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&
+	sudo apt-get install -y nodejs
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
+rm nvim-linux64.tar.gz
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim/
 rm -rf ~/.config/nvim/.git
