9780194ce5f6e530f5958a289f58bb8820b4fd65
James Robinson <james@ember-climate.org>
2024-03-20 23:36:52 +0000

    add dagster warnings to try/catch

--- config/.ipython/profile_default/startup/disable-warnings.py
+++ config/.ipython/profile_default/startup/disable-warnings.py
@@ -1,6 +1,11 @@
 import warnings
 
-import dagster as dag
-
-warnings.filterwarnings("ignore", category=dag.ExperimentalWarning)
 warnings.filterwarnings("ignore", category=RuntimeWarning, module="pandasdmx")
+
+
+try:
+    import dagster as dag
+
+    warnings.filterwarnings("ignore", category=dag.ExperimentalWarning)
+except ModuleNotFoundError:
+    pass
