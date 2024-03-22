7d63e58a9afd8c9e6ad28f13af285f2a755f3f43
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-03-22 22:27:38 +0000

    add line to create config dir

--- scripts/install/apps/tmux.sh
+++ scripts/install/apps/tmux.sh
@@ -1,4 +1,5 @@
 sudo apt install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+mkdir -p $HOME/.config/tmux/
 ln $DOTFILES/config/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
