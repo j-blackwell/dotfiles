b67231367aac551c3a43de2de3197284dbf046e7
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-04-04 12:19:28 +0100

    add gnome theme

--- scripts/install/gnome/apps.sh
+++ scripts/install/gnome/apps.sh
@@ -2,3 +2,7 @@
 
 sudo apt install gnome-sushi
 flatpak install flathub com.github.GradienceTeam.Gradience
+
+# terminal theme
+curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.2.0/install.py | python3 -
+
