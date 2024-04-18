96dc70bec28fb30affa3e7e12b8735e78f57f3a0
Work <james@pop-os.home>
2024-04-18 11:41:46 +0100

    ensure symlinks

--- scripts/install/setup/setup.sh
+++ scripts/install/setup/setup.sh
@@ -4,24 +4,20 @@
 sudo apt install flatpak
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-
 ## automatic updates
 sudo apt install unattended-upgrades
 sudo dpkg-reconfigure unattended-upgrades
-
 
 ## snap
 sudo apt update
 sudo apt install snapd
 
 ## .bashrc
-ln $DOTFILES/config/.bashrc $HOME/.bashrc
-ln $DOTFILES/config/.gitconfig $HOME/.gitconfig
-ln $DOTFILES/config/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
-ln $DOTFILES/config/.ipython/profile_default/ipython_config.py $HOME/.ipython/profile_default/ipython_config.py
-ln $DOTFILES/config/.ipython/profile_default/startup/disable-warnings.py $HOME/.ipython/profile_default/startup/disable-warnings.py
-
-
+ln -s $DOTFILES/config/.bashrc $HOME/.bashrc
+ln -s $DOTFILES/config/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/config/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln -s $DOTFILES/config/.ipython/profile_default/ipython_config.py $HOME/.ipython/profile_default/ipython_config.py
+ln -s $DOTFILES/config/.ipython/profile_default/startup/disable-warnings.py $HOME/.ipython/profile_default/startup/disable-warnings.py
 
 ## TODO: folders
 #ln $DOTFILES/config/.config/synth-shell
