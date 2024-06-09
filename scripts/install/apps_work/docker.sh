79c3fbd06989bb9eeee9a0322df482f1aa5d19be
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-06-09 22:51:33 +0100

    rework for stow

--- scripts/install/apps_work/docker.sh
+++ scripts/install/apps_work/docker.sh
@@ -10,9 +10,14 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
 # install
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# add docker group and users to group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
---
+++ scripts/install/set_display.sh
@@ -0,0 +1,1 @@
+wlr-randr --output DP-1 --mode 1920x1080@165.003006 --pos 0,0 --output HDMI-A-1 --mode 1920x1080 --pos -1920,0
