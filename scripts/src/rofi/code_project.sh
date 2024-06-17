f7008ea395d59a09a7cc150c566996b9214abb52
James Robinson <james@ember-climate.org>
2024-06-17 22:31:14 +0100

    update code projects rofi to just show last directory name

--- scripts/src/rofi/code_project.sh
+++ scripts/src/rofi/code_project.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-function code_projects()
-{
-    find ~/code ~/code-personal ~/dotfiles/nvim/.config -mindepth 1 -maxdepth 1 -type d | xargs -I {} echo {} ; echo ~/dotfiles
+function code_projects() {
+    find ~/code ~/code-personal ~/dotfiles/nvim/.config -mindepth 1 -maxdepth 1 -type d | xargs -I {} basename {} | xargs -I {} echo {}
+    echo dotfiles
 }
 
 CODE_PROJECT=$( (code_projects) | rofi -dmenu -p "Select code project")
@@ -10,6 +10,6 @@ CODE_PROJECT=$( (code_projects) | rofi -dmenu -p "Select code project")
 if [[ -z "${CODE_PROJECT}" ]]; then
     echo "Cancel"
 else
-    DIRECTORY=$(basename "${CODE_PROJECTS}")
-    rofi-sensible-terminal -e tmux new-session -A -s $CODE_PROJECT "cd $CODE_PROJECT && source ~/.bashrc && /opt/nvim-linux64/bin/nvim .; bash" &
+    FULL_PATH=$(find ~/code ~/code-personal ~/dotfiles/nvim/.config -mindepth 1 -maxdepth 1 -type d | grep "/${CODE_PROJECT}$" || echo ~/dotfiles)
+    rofi-sensible-terminal -e tmux new-session -A -s $CODE_PROJECT "cd $FULL_PATH && source ~/.bashrc && /opt/nvim-linux64/bin/nvim .; bash" &
 fi
