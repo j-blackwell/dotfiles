3223e5a9525933b68e1e679323719524a5c26a18
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-07-08 14:21:24 +0100

    sort out audio on archq

--- scripts/install/arch/install_arch.sh
+++ scripts/install/arch/install_arch.sh
@@ -1,5 +1,5 @@
 # utils
-sudo pacman -S wl-clipboard reflector pacman-contrib xdg-desktop-portal xdg-desktop-portal-wlr grim ripgrep amd-ucode jq fd
+sudo pacman -S wl-clipboard reflector pacman-contrib xdg-desktop-portal xdg-desktop-portal-wlr grim ripgrep amd-ucode jq fd btop alsa-utils playerctl
 
 sudo systemctl enable reflector.service && sudo systemctl start reflector.service
 sudo systemctl enable fstrim.timer
@@ -18,7 +18,10 @@ systemctl --user start xdg-desktop-portal-wlr.service
 sudo pacman -S upower
 
 # programming
-sudo pacman -S npm python3 python-pip python-virtualenv rust
+sudo pacman -S npm python3 python-pip python-virtualenv rust pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 # fonts
 sudo pacman -S ttf-martian-mono-nerd ttf-font-awesome noto-fonts-emoji
@@ -53,14 +56,16 @@ sudo pacman -S pulseaudio lib32-libpulse lib32-alsa-plugins mpv
 sudo systemctl --user enable dbus
 sudo systemctl --user enable pulseaudio
 
+pactl set-default-sink alsa_output.pci-0000_26_00.1.hdmi-stereo-extra4
+
 # desktop
 sudo pacman -S rofi polybar
 
 # apps
-sudo pacman -S firefox flameshot lazygit nautilus neovim tmux
+sudo pacman -S firefox flameshot lazygit nautilus neovim tmux gimp
 yay -S ytmdesktop-bin
 
 # work
-sudo pacman -S docker mysql-workbench r
-yay -S google-chrome dropbox duckdb-bin google-cloud-cli slack-desktop
+sudo pacman -S docker mysql-workbench r libreoffice-fresh
+yay -S google-chrome dropbox duckdb-bin google-cloud-cli slack-desktop dropbox-cli
 
