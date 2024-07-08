fbddb374cacea8046dd1a7bff6c58d0c1b661fbb
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-07-08 14:22:11 +0100

    improvements to rofi code projects

--- scripts/src/rofi/code_project.sh
+++ scripts/src/rofi/code_project.sh
@@ -11,5 +11,5 @@ if [[ -z "${CODE_PROJECT}" ]]; then
     echo "Cancel"
 else
     FULL_PATH=$(find ~/code ~/code-personal ~/dotfiles/nvim/.config -mindepth 1 -maxdepth 1 -type d | grep "/${CODE_PROJECT}$" || echo ~/dotfiles)
-    rofi-sensible-terminal -e tmux new-session -A -s $CODE_PROJECT "cd $FULL_PATH && source ~/.bashrc && /opt/nvim-linux64/bin/nvim .; bash" &
+    rofi-sensible-terminal -e tmux new-session -A -s $CODE_PROJECT "cd $FULL_PATH && nvim .; bash" &
 fi
--- scripts/src/rofi/specific_code_project.sh
+++ scripts/src/rofi/specific_code_project.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 CODE_PROJECT=$1
+TMUX_NAME=$(basename $CODE_PROJECT)
 
 if [[ -z "$CODE_PROJECT" ]]; then
     echo "No project set"
 else
-    i3-sensible-terminal -e tmux new-session -A -s $CODE_PROJECT "cd $CODE_PROJECT && /opt/nvim-linux64/bin/nvim .; bash" &
+    i3-sensible-terminal -e tmux new-session -A -s $TMUX_NAME "cd $CODE_PROJECT && nvim .; bash" &
 fi
