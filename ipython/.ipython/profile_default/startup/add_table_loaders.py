bd85c77b9d89eb934569fc4f8552a009f119d417
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-12-06 16:39:58 +0000

    add res_tracker table loader

--- ipython/.ipython/profile_default/startup/add_table_loaders.py
+++ ipython/.ipython/profile_default/startup/add_table_loaders.py
@@ -1,11 +1,13 @@
 try:
     import src.helpers.ipython as i
+
     cm = i.create_schema_loader("capacity", globals=globals())
     gy = i.create_schema_loader("generation_yearly", globals=globals())
     gm = i.create_schema_loader("generation_monthly", globals=globals())
     gh = i.create_schema_loader("generation_hourly", globals=globals())
     ga = i.create_schema_loader("google_analytics", globals=globals())
     p = i.create_schema_loader("price", globals=globals())
+    rt = i.create_schema_loader("res_tracker", globals=globals())
     s = i.create_schema_loader("sources", globals=globals())
 except ImportError:
     pass
