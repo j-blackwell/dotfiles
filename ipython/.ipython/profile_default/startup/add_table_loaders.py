e93cfde9d1542a46a8e8111b18dad25ac3dcfddf
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-02-17 11:37:07 +0000

    add polars and pandas imports to ipython

--- ipython/.ipython/profile_default/startup/add_table_loaders.py
+++ ipython/.ipython/profile_default/startup/add_table_loaders.py
@@ -2,12 +2,24 @@ try:
     import src.helpers.ipython as i
 
     cm = i.create_schema_loader("capacity", globals=globals())
+    cy = i.create_schema_loader("capacity_yearly", globals=globals())
     gy = i.create_schema_loader("generation_yearly", globals=globals())
     gm = i.create_schema_loader("generation_monthly", globals=globals())
     gh = i.create_schema_loader("generation_hourly", globals=globals())
     ga = i.create_schema_loader("google_analytics", globals=globals())
+    m = i.create_schema_loader("methane", globals=globals())
     p = i.create_schema_loader("price", globals=globals())
     rt = i.create_schema_loader("res_tracker", globals=globals())
     s = i.create_schema_loader("sources", globals=globals())
+except ImportError:
+    pass
+
+try:
+    import polars as pl
+except ImportError:
+    pass
+
+try:
+    import pandas as pd
 except ImportError:
     pass
