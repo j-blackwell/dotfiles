464a18dccf42ec476d6c227c2c003af79e67ca3f
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-03-06 13:43:41 +0000

    add print_pl helper

--- ipython/.ipython/profile_default/startup/add_table_loaders.py
+++ ipython/.ipython/profile_default/startup/add_table_loaders.py
@@ -16,6 +16,10 @@ except ImportError:
 
 try:
     import polars as pl
+
+    def print_pl(df: pl.DataFrame, tbl_rows=None, tbl_cols=None):
+        with pl.Config(tbl_rows=tbl_rows, tbl_cols=tbl_cols):
+            print(df)
 except ImportError:
     pass
 
