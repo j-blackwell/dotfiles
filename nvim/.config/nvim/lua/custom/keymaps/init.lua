fde5fbc8cecf7c85ba414db57dbe6f79a2e7a222
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-03-31 18:39:02 +0100

    add delete all buffers with force

--- nvim/.config/nvim/lua/custom/keymaps/init.lua
+++ nvim/.config/nvim/lua/custom/keymaps/init.lua
@@ -2,7 +2,6 @@ local function map(mode, lhs, rhs, opts)
 	vim.keymap.set(mode, lhs, rhs, opts or {})
 end
 
-
 return {
 	map("n", "Q", "@qj", { desc = "Apply the 'q' macro and move to the next line" }),
 	map("n", "J", "Vj", { desc = "Select next line in visual block mode" }),
@@ -16,6 +15,7 @@ return {
 	map("n", "<Leader>d%", ":call delete(@%)<CR>:bp<bar>bd#<CR>", { desc = "[D]elete the file of the current buffer" }),
 	map("n", "<Leader>bd", "<CMD>bd<CR>", { desc = "[B]uffer [d]elete" }),
 	map("n", "<Leader>ba", "<CMD>bufdo bwipeout<CR>", { desc = "[B]uffer delete [a]ll" }),
+	map("n", "<Leader>bA", "<CMD>bufdo bwipeout!<CR>", { desc = "[B]uffer delete [A]ll (force)" }),
 	map("n", "<Leader>cL", ":LspStop<CR>:LspStart<CR>", { desc = "Restart the [L]SP" }),
 	map("n", "<Leader>ll", ":Lazy<CR>", { desc = "Load [L]azy" }),
 	map("n", "<S-h>", "<CMD>bprev<CR>", { desc = "Switch to previous buffer" }),
@@ -24,6 +24,6 @@ return {
 	map("n", "vag", "ggVG", { desc = "Entire file" }),
 	map("n", "<Leader>rr", "diwcf=return <Esc>", { desc = "Replace assignment with return" }),
 	map("n", "<Leader>ra", "ciw=<Esc>i", { desc = "Replace return with assignment" }),
-	map("n", "<Leader>rd", "^sd\"f:r=", { desc = "Replace dictionary entry with assignment" }),
+	map("n", "<Leader>rd", '^sd"f:r=', { desc = "Replace dictionary entry with assignment" }),
 	map("i", "<C-u>", "", { desc = "Remove ctrl+u" }),
 }
