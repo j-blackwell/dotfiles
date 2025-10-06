5100ebada6c14201c33df1978ce27cbf2a45c7af
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-10-06 12:49:35 +0100

    feat(hyprland): add screenshot script and binding

---
+++ scripts/src/rofi/screenshot_full.py
@@ -0,0 +1,11 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+# ]
+# ///
+
+import subprocess
+
+if __name__ == "__main__":
+    command = "grim -c - | swappy -f -"
+    subprocess.run(["bash", "-c", command])
