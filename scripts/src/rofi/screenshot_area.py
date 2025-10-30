f42cdc22bcef4aab10054180cdd0fecafcf5a8cc
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-10-30 14:05:19 +0000

    feat(hyprland): split screenshot with copy vs with swappy

--- scripts/src/rofi/screenshot_area.py
+++ scripts/src/rofi/screenshot_area.py
@@ -1,11 +1,28 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
+#     "typer",
 # ]
 # ///
 
 import subprocess
 
-if __name__ == "__main__":
-    command = 'grim -g "$(slurp)" - | tee >(wl-copy) | swappy -f -'
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def copy():
+    command = 'grim -g "$(slurp)" - | tee >(wl-copy) && notify-send --app-name "Screenshot" "Screenshot" "Screenshot copied to clipboard!"'
     subprocess.run(["bash", "-c", command])
+
+
+@app.command()
+def swappy():
+    command = 'grim -g "$(slurp)" - | swappy -f -'
+    subprocess.run(["bash", "-c", command])
+
+
+if __name__ == "__main__":
+    app()
