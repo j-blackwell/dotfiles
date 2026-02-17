edea7d3879995abc7564b657fbea5099eddd77fc
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2026-02-17 12:31:16 +0000

    fix(nvim): treesitter python

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -3,10 +3,16 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
+			-- Configure to override system parsers
+			local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter_parsers"
+			vim.fn.mkdir(parser_install_dir, "p")
+			vim.opt.runtimepath:prepend(parser_install_dir)
+
 			-- Prefer git instead of curl in order to improve connectivity in some environments
 			require("nvim-treesitter.install").prefer_git = true
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
+				parser_install_dir = parser_install_dir,
 				ensure_installed = {
 					"bash",
 					"c",
